70cd16899a4f6e305379662360098ca80dea7894
Nicolas Perraut <n.perraut@gmail.com>
2018-03-23 06:21:37 +0100

    InCluster config

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -13,12 +14,21 @@ import (
 // Initialize connection to Kubernetes API and start watching for services
 func main() {
 	var kubeConfig *string
+	var inCluster *bool
 	kubeConfig = flag.String("kubeConfig", filepath.Join(".", "config"), "(optional) absolute path to the kube config file")
+	inCluster = flag.Bool( "inCluster", false, "Use InCluster config")
 
 	flag.Parse()
 
+	var config *rest.Config
+	var err error
+
 	// use the current context in kubeConfig
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeConfig)
+	if !*inCluster {
+		config, err = clientcmd.BuildConfigFromFlags("", *kubeConfig)
+	} else {
+		config, err = rest.InClusterConfig()
+	}
 	if err != nil {
 		panic(err.Error())
 	}
